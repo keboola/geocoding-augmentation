914d01a855dd73226abd3af13e4fa1bb534aa940
Jakub Matejka <jakub@keboola.com>
2014-11-25 17:27:39 +0000

    feat: init

---
+++ Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Keboola\GeocodingBundle
+\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('KeboolaGeocodingBundle
+:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ KeboolaGeocodingBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Keboola\GeocodingBundle
+;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KeboolaGeocodingBundle
+ extends Bundle
+{
+}
