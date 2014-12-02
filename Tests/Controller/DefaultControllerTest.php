6a3953f7b4c15398768407e48b7afa4de31c1bf1
Jakub Matejka <jakub@keboola.com>
2014-12-02 10:28:43 +0000

    test: setup test environment

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -1,18 +1,20 @@
 <?php
 
-namespace Keboola\GeocodingBundle
-\Tests\Controller;
+namespace Keboola\GeocodingBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class DefaultControllerTest extends WebTestCase
+class DefaultControllerTest extends AbstractControllerTest
 {
+
+    protected function runAugmentation($tableId, $column)
+    {
+        $this->processJob('ag-geocoding/run', array(
+            'tableId' => $tableId,
+            'column' => $column
+        ));
+    }
+
     public function testIndex()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->runAugmentation('out.c-main.users', 'address');
     }
 }
