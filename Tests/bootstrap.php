f9719b19de0ea80d94ea376b3102e0895dba1602
Jakub Matejka <jakub@keboola.com>
2014-12-15 17:13:19 +0000

    test: read environmental variables if set

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,4 +9,30 @@ if (file_exists(__DIR__ . '/config.php')) {
 	require_once __DIR__ . '/config.php';
 }
 
+defined('STORAGE_API_URL')
+|| define('STORAGE_API_URL', getenv('STORAGE_API_URL') ? getenv('STORAGE_API_URL') : 'https://connection.keboola.com');
+
+defined('STORAGE_API_TOKEN')
+|| define('STORAGE_API_TOKEN', getenv('STORAGE_API_TOKEN') ? getenv('STORAGE_API_TOKEN') : 'your_token');
+
+defined('GOOGLE_KEY')
+|| define('GOOGLE_KEY', getenv('GOOGLE_KEY') ? getenv('GOOGLE_KEY') : 'your_google_api_key');
+
+defined('MAPQUEST_KEY')
+|| define('MAPQUEST_KEY', getenv('MAPQUEST_KEY') ? getenv('MAPQUEST_KEY') : 'your_mapquest_key');
+
+defined('DB_HOST')
+|| define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : '127.0.0.1');
+
+defined('DB_NAME')
+|| define('DB_NAME', getenv('DB_NAME') ? getenv('DB_NAME') : 'ag_geocoding');
+
+defined('DB_USER')
+|| define('DB_USER', getenv('DB_USER') ? getenv('DB_USER') : 'user');
+
+defined('DB_PASSWORD')
+|| define('DB_PASSWORD', getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : '');
+
+
+
 require_once __DIR__ . '/../vendor/autoload.php';
