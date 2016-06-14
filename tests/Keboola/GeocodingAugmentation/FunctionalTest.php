35e65c91adf8e1b37fdf5338d6f6b8a5c9cdab20
Jakub Matejka <jakub@keboola.com>
2016-06-14 13:30:03 +0200

    test: create empty output table if input is empty

--- tests/Keboola/GeocodingAugmentation/FunctionalTest.php
+++ tests/Keboola/GeocodingAugmentation/FunctionalTest.php
@@ -64,4 +64,54 @@ class FunctionalTest extends \PHPUnit_Framework_TestCase
 
         $this->assertFileExists("{$temp->getTmpFolder()}/out/tables/geocoding.csv");
     }
+
+    public function testFunctionalEmptyInput()
+    {
+        $temp = new Temp();
+        $temp->initRunFolder();
+
+        file_put_contents($temp->getTmpFolder() . '/config.yml', Yaml::dump([
+            'storage' => [
+                'input' => [
+                    'tables' => [
+                        [
+                            'source' => 'in.c-main.coordinates',
+                            'destination' => 'coordinates.csv'
+                        ]
+                    ]
+                ],
+                'output' => [
+                    'tables' => [
+                        [
+                            'destination' => 'in.c-main.geocoding',
+                            'source' => 'geocoding.csv'
+                        ]
+                    ]
+                ]
+            ],
+            'parameters' => [
+                'method' => 'reverse',
+                'provider' => 'openstreetmap',
+                'outputTable' => 'geocoding.csv',
+                'inputTables' => [
+                    'coordinates.csv'
+                ]
+            ]
+        ]));
+
+        mkdir($temp->getTmpFolder().'/in');
+        mkdir($temp->getTmpFolder().'/in/tables');
+        file_put_contents($temp->getTmpFolder().'/in/tables/coordinates.csv', 'lat,lon');
+        copy(__DIR__ . '/coordinates.csv.manifest', $temp->getTmpFolder().'/in/tables/coordinates.csv.manifest');
+
+        $process = new Process("php ".__DIR__."/../../../src/run.php --data=".$temp->getTmpFolder());
+        $process->setTimeout(null);
+        $process->run();
+        if (!$process->isSuccessful()) {
+            $this->fail($process->getOutput().PHP_EOL.$process->getErrorOutput());
+        }
+
+        $this->assertFileExists("{$temp->getTmpFolder()}/out/tables/geocoding.csv");
+        $this->assertCount(1, file("{$temp->getTmpFolder()}/out/tables/geocoding.csv"));
+    }
 }
